7af55b21d39845ff970a002e29b78f5d27fb71a4
ChippyDev <52300375+ChippyDev@users.noreply.github.com>
2020-08-27 22:25:50 -0400

    math

--- commands/math.js
+++ commands/math.js
@@ -4,11 +4,51 @@ module.exports = {
 	secret: true,
 	description: 'do math',
 	execute(message, args) {
-		let input = '';
-		for (let loop = 0; loop < args.length; loop++) { input += args[loop]; }
-		console.log(imput);
-		for (let loop = 0; loop < imput.length; loop++) {
-            
-        }
+		var input = '';
+		const numbers = '0123456789';
+		const operators = '+-*/';
+
+		let firstOperand = '';
+		let operator = -1;
+		let secondOperand = '';
+		let operatorFound = false;
+		let result = 0;
+
+		for (let loop = 0; loop < args.length; loop++) {
+			input += args[loop];
+		}
+		console.log(input);
+		for (let loop = 0; loop < input.length; loop++) {
+			if (numbers.indexOf(input[loop]) >= 0) {
+				if ((operatorFound = false)) {
+					firstOperand += input[loop];
+					console.log(firstOperand);
+				} else {
+					secondOperand += input[loop];
+					console.log(secondOperand);
+				}
+			} else if (operators.indexOf(input[loop]) >= 0) {
+				if ((operatorFound = false)) {
+					operator = operators.indexOf(input[loop]);
+					operatorFound = true;
+				} else {
+					switch (operator) {
+						case -0:
+							console.log('error -0 operator');
+							break;
+						case 0:
+							result = firstOperand + secondOperand;
+							break;
+						case 1:
+						case 2:
+						case 3:
+							console.log('noodles');
+					}
+				}
+			} else {
+				console.log('its invalid!');
+			}
+			message.channel.send(result);
+		}
 	},
 };
--- commands/test.js
+++ commands/test.js
@@ -7,15 +7,7 @@ module.exports = {
 	secret: true,
 	description: 'A debugging command, only works for Chippy',
 	execute(message, args) {
-		if (message.author.id == '140752312040816640') {
-			let cleanArgs = '';
-
-			for (let a = 0; a < args.length; a++) {
-				cleanArgs += args[a];
-			}
-			// console.log(cleanArgs);
-
-			console.log(cleanArgs.indexOf('+'));
-		}
+		let x = 'happy'
+		console.log(x[0])
 	},
 };
